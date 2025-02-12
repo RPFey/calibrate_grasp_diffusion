3d51e3ba9ff3a0dccdc9de317a78edb2a211ffb4
Wen Jiang <wenjiang@seas.upenn.edu>
2025-02-12 02:57:51 +0000

    enable torch grad for score computation

--- se3dif/samplers/grasp_samplers.py
+++ se3dif/samplers/grasp_samplers.py
@@ -135,12 +135,13 @@ class Grasp_AnnealedLD():
             H0_in = SO3_R3(R=H1[:,:3,:3], t=H1[:,:3, -1])
             phi0 = H0_in.log_map()
 
-            ## 2. Compute energy gradient ##
-            phi0_in = phi0.detach().requires_grad_(True)
-            H_in = SO3_R3().exp_map(phi0_in).to_matrix()
-            t_in = phase*torch.ones_like(H_in[:,0,0])
-            e = self.model(H_in, t_in)
-            d_phi = torch.autograd.grad(e.sum(), phi0_in)[0]
+            with torch.enable_grad():
+                ## 2. Compute energy gradient ##
+                phi0_in = phi0.detach().requires_grad_(True)
+                H_in = SO3_R3().exp_map(phi0_in).to_matrix()
+                t_in = phase*torch.ones_like(H_in[:,0,0])
+                e = self.model(H_in, t_in)
+                d_phi = torch.autograd.grad(e.sum(), phi0_in)[0]
 
             ## 3. Compute noise vector ##
             if noise_off:
