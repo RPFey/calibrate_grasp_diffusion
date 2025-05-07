0f3cad6538fdec764cbb8129b6d2d1ee623b7a72
Boshu Lei <leiboshu@seas.upenn.edu>
2025-05-07 16:23:21 +0000

    add pc sampler

--- se3dif/samplers/grasp_samplers.py
+++ se3dif/samplers/grasp_samplers.py
@@ -111,7 +111,7 @@ class Grasp_AnnealedLD():
         self.model = model
         self.device = device
         self.dim = dim
-        self.shape = [4,4]
+        self.shape = [4, 4]
         self.batch = batch
         self.enable_time = enable_time
 
@@ -128,20 +128,20 @@ class Grasp_AnnealedLD():
         # compute the update step size
         if self.enable_time:
             ## Phase
-            noise_std = .5
+            noise_std = .5 # .1
             eps = self.model.final_t # 1e-3
             phase = ((self.T - t) / (self.T)) + eps
             sigma_T = self._marginal_prob_std(eps)
 
             ## Annealed Langevin Dynamics ##
-            alpha = 1e-3
+            alpha = 1e-3 # 7e-4
             sigma_i = self._marginal_prob_std(phase)
             ratio = sigma_i ** 2 / sigma_T ** 2
             c_lr = alpha * ratio
             
         if noise_off:
-            phase = eps
-            c_lr = max(0.1 * np.cos( (np.pi / 2) * t / self.T_fit), 1e-3) if self.enable_time else 0.05
+            phase = self.model.final_t # + 1 / (2 * self.T)
+            c_lr = 0.003 # cmax(0.05 * np.cos( (np.pi / 2) * t / self.T_fit), 1e-3) if self.enable_time else 0.05
 
         H1 = H0
         for k in range(self.k_steps):
@@ -166,6 +166,120 @@ class Grasp_AnnealedLD():
 
             ## 4. Compute translation ##
             delta = -c_lr/2 * d_phi + np.sqrt(c_lr) * noise
+            w_Delta = SO3().exp_map(delta[:, 3:])
+            t_delta = delta[:, :3]
+
+            ## 5. Move the points ##
+            R1_out = th.compose(w_Delta, H0_in.R)
+            t1_out = H0_in.t + t_delta
+            H1 = SO3_R3(R=R1_out, t=t1_out).to_matrix()
+
+        return H1
+
+    def sample(self, save_path=False, batch=None, H0=None):
+        ## 1.Sample initial SE(3) ##
+        if batch is None:
+            batch = self.batch
+            
+        if H0 is None:
+            H0 = SO3_R3().sample(batch).to(self.device, torch.float32)
+
+        ## 2.Langevin Dynamics (We evolve the data as [R3, SO(3)] pose)##
+        Ht = H0
+        if save_path:
+            trj_H = Ht[None, ...]
+        
+        for t in range(self.T):
+            Ht = self._step(Ht, t, noise_off=self.deterministic)
+            if save_path:
+                trj_H = torch.cat((trj_H, Ht[None,:]), 0)
+        
+        for t in range(self.T_fit):
+            Ht = self._step(Ht, t, noise_off=True)
+            if save_path:
+                trj_H = torch.cat((trj_H, Ht[None,:]), 0)
+
+        # compute final score 
+        with torch.no_grad():
+            # eps = 1e-3
+            t_in = self.model.final_t * torch.ones_like(Ht[:, 0, 0]) \
+                        if self.enable_time else None
+            energy = self.model(Ht, t_in) 
+            
+        if save_path:
+            return Ht, energy, trj_H
+        else:
+            return Ht, energy
+        
+class Grasp_PCSampler():
+    def __init__(self, model, device='cpu', batch=10, dim =3, k_steps=1,
+                 T=200, T_fit=5, deterministic=False, enable_time=True):
+
+        self.model = model
+        self.device = device
+        self.dim = dim
+        self.shape = [4, 4]
+        self.batch = batch
+        self.enable_time = enable_time
+
+        ## Langevin Dynamics evolution ##
+        self.T = T
+        self.T_fit = T_fit
+        self.k_steps = k_steps
+        self.deterministic = deterministic
+
+    def _marginal_prob_std(self, t, sigma=0.5):
+        return np.sqrt((sigma ** (2 * t) - 1.) / (2. * np.log(sigma)))
+
+    def _step(self, H0, t, noise_off=True):
+        # compute the update step size
+        if self.enable_time:
+            ## Phase
+            noise_std = .1
+            eps = self.model.final_t # 1e-3
+            phase = ((self.T - t) / (self.T)) + eps
+            sigma_i = self._marginal_prob_std(phase)
+            
+            phase_T_1 = max( ((self.T - (t + 1)) / (self.T)) + eps, eps)
+            sigma_T_1 = self._marginal_prob_std(phase_T_1)
+            c_lr = (sigma_i**2 - sigma_T_1**2)
+
+            # Corrector Step Size
+            alpha = 5e-3
+
+        if noise_off:
+            phase = self.model.final_t # + 1 / (2 * self.T)
+            c_lr = 0.003 # max(0.05 * np.cos( (np.pi / 2) * t / self.T_fit), 1e-3) if self.enable_time else 0.05
+
+        H1 = H0
+        for k in range(self.k_steps):
+
+            ## 1.Set input variable to Theseus ##
+            H0_in = SO3_R3(R=H1[:,:3,:3], t=H1[:,:3, -1])
+            phi0 = H0_in.log_map()
+
+            with torch.enable_grad():
+                ## 2. Compute energy gradient ##
+                phi0_in = phi0.detach().requires_grad_(True)
+                H_in = SO3_R3().exp_map(phi0_in).to_matrix()
+                t_in = phase * torch.ones_like(H_in[:,0,0]) if self.enable_time else None
+                e = self.model(H_in, t_in)
+                d_phi = torch.autograd.grad(e.sum(), phi0_in)[0]
+
+            ## 3. Compute noise vector ##
+            if noise_off:
+                noise = torch.zeros_like(phi0_in)
+            else:
+                noise = torch.randn_like(phi0_in) * noise_std
+
+            ## 4. Compute translation ##
+            if k == 0:
+                # Predictor Step
+                delta = -c_lr * d_phi + np.sqrt(c_lr) * noise
+            else:
+                # Corrector Step
+                delta = -alpha * d_phi + np.sqrt(2 * alpha) * noise
+
             w_Delta = SO3().exp_map(delta[:, 3:])
             t_delta = delta[:, :3]
 
